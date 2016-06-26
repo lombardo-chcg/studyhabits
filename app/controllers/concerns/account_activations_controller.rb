25ecbf7ac3a01aca1bb0f3142d51ec9cc1f17152
lombardo-chcg <dpa.lombardo@gmail.com>
2016-06-26 00:48:32 -0500

    Tidy up more more issues to get mailer working

--- app/controllers/concerns/account_activations_controller.rb
+++ app/controllers/concerns/account_activations_controller.rb
@@ -1,7 +1,7 @@
 class AccountActivationsController < ApplicationController
 
   def edit
-    user = User.find_by(email: params[email])
+    user = User.find_by(email: params[:email])
     if user && !user.activated?
       user.update_attributes(activated: true, activation_datetime: Time.now)
       render :json => {userId: user.id, userName: user.username}
