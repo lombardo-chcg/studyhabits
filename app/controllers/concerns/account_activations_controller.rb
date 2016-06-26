0048e38aef2cb9058d90617815a8fb16d0fd391e
lombardo-chcg <dpa.lombardo@gmail.com>
2016-06-26 00:42:31 -0500

    Tidy up more issues to get mailer working

--- app/controllers/concerns/account_activations_controller.rb
+++ app/controllers/concerns/account_activations_controller.rb
@@ -1,9 +1,13 @@
-def edit
-  user = User.find_by(email: params[email])
-  if user && !user.activated?
-    user.update_attributes(activated: true)
-    render :json => {userId: user.id, userName: user.username}
-  else
-    render :json => {errors: ['invalid link']}
+class AccountActivationsController < ApplicationController
+
+  def edit
+    user = User.find_by(email: params[email])
+    if user && !user.activated?
+      user.update_attributes(activated: true, activation_datetime: Time.now)
+      render :json => {userId: user.id, userName: user.username}
+    else
+      render :json => {errors: ['invalid link']}
+    end
   end
+
 end
