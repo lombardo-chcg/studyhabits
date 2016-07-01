05fa6f7ff5f616a174f1cafe42e5a427ec3d98fe
lombardo-chcg <dpa.lombardo@gmail.com>
2016-07-01 14:22:13 -0500

    Add template for confirming new users

---
+++ app/assets/javascripts/components/email_validated.jsx
@@ -0,0 +1,8 @@
+var EmailValidated = React.createClass({
+  render: function() {
+    return <div>
+      <p>You email has been validated!  Please proceed to login.</p>
+      <a href='/'>login here!</a>
+    </div>
+  }
+})
